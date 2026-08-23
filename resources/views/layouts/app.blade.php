c9c6088fabfb7b3d75dd968303e2e8601ce0de23
Jairo Roosenburg <110334823+jairorose@users.noreply.github.com>
2026-08-23 15:30:51 +0200

    feat: implemented sidebar menu for navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,15 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css'); }}">
     </head>
     <body>
-        @yield('content')
+        <div class="side-menu">
+            <div class="logo">StockPilot</div>
+            <nav>
+                <a href="" class="nav-link">Dashboard</a>
+                <a href="{{ route('items.index') }}" class="nav-link">Items</a>
+            </nav>
+        </div>
+        <div class="content-container">
+            @yield('content')
+        </div>
     </body>
 </html>
---
+++ resources/views/layouts/standard.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>StockPilot - @yield('title')</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/normalize.css'); }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css'); }}">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
